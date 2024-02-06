d5fdf378e23f08e05a407ff4e83e1df4cfc1ef82
Neebelwerfer <jakob.hedeby@gmail.com>
2024-02-06 12:11:06 +0100

    Renamed a couple of things

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MovementController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public InputActionReference Movement;
 
