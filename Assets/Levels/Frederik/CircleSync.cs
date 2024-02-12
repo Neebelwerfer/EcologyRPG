ba5b961e12ffbf62d7789ccd9b0e6701bd5e5a78
Neebelwerfer <jakob.hedeby@gmail.com>
2024-02-12 16:55:31 +0100

    Added camera to player prefab

--- Assets/Levels/Frederik/CircleSync.cs
+++ Assets/Levels/Frederik/CircleSync.cs
@@ -1,7 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public class SeeThroughMaterial
+{
+    public Material material;
+    public int counter;
+
+    public SeeThroughMaterial(Material material, float CircleSize)
+    {
+        this.material = material;
+        material.SetFloat(CircleSync.SizeID, CircleSize);
+        counter = 1;
+    }
+}
 
 public class CircleSync : MonoBehaviour
 {
@@ -13,49 +27,74 @@ public class CircleSync : MonoBehaviour
     public Camera Camera;
     public LayerMask Mask;
 
-    List<Material> _Materials;
+    Dictionary<Collider, SeeThroughMaterial> _MaterialsByName;
+
+    RaycastHit[] hits;
 
     private void Start()
     {
-        _Materials = new List<Material>();
+        _MaterialsByName = new Dictionary<Collider, SeeThroughMaterial>(10);
+        hits = new RaycastHit[10];
         Camera = Camera.main;
     }
 
     void Update()
     {
+        DecreaseCounter();
         var dir = Camera.transform.position - transform.position;
         Ray ray = new Ray(transform.position, dir.normalized);
 
-        ClearMaterialList();
-        var hits = Physics.SphereCastAll(ray, SphereCastRadius, dir.magnitude - (SphereCastRadius * 2), Mask);
+        var numHits = Physics.SphereCastNonAlloc(ray, SphereCastRadius, hits, dir.magnitude - (SphereCastRadius * 2), Mask);
         
-        if(hits.Length > 0)
+        if(numHits > 0)
         {
-            foreach(var hit in hits)
+            for (int i = 0; i < numHits; i++)
             {
-                Material mat = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
-                _Materials.Add(mat);
-                mat.SetFloat(SizeID, CircleSize);
+                var hit = hits[i];
+                if (hit.collider == null) continue;
+                if (!_MaterialsByName.ContainsKey(hit.collider))
+                {
+                    Material mat = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
+                    _MaterialsByName.Add(hit.collider, new SeeThroughMaterial(mat, CircleSize));
+                }
+                else
+                {
+                    _MaterialsByName[hit.collider].counter += 1;
+                }
             }
         }
+        ClearMaterialList();
         UpdateMatPos();
+    }
+
+    private void DecreaseCounter()
+    {
+        foreach (var col in _MaterialsByName.Keys)
+        {
+            _MaterialsByName[col].counter -= 1;
+        }
     }
 
     private void ClearMaterialList()
     {
-        foreach(var mat in _Materials)
+        var keys = new List<Collider>(_MaterialsByName.Keys);
+        foreach (var col in keys)
         {
-            mat.SetFloat(SizeID, 0);
+            if (_MaterialsByName[col].counter == 0)
+            {
+                _MaterialsByName[col].material.SetFloat(SizeID, 0);
+                _MaterialsByName.Remove(col);
+            }
         }
-        _Materials.Clear();
     }
 
     private void UpdateMatPos()
     {
-        foreach(var mat in _Materials)
+        var view = Camera.WorldToViewportPoint(transform.position);
+
+        foreach (var (col, stMat) in _MaterialsByName)
         {
-            var view = Camera.WorldToViewportPoint(transform.position);
-            mat.SetVector(PosID, view);
+            stMat.material.SetVector(PosID, view);
         }
     }
 }
