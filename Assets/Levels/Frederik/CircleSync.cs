3c893c4f63fec0619081e8263924191c4cacae93
Neebelwerfer <jakob.hedeby@gmail.com>
2024-02-12 12:46:56 +0100

    Made See through works through multiple wall

--- Assets/Levels/Frederik/CircleSync.cs
+++ Assets/Levels/Frederik/CircleSync.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +7,55 @@ public class CircleSync : MonoBehaviour
 {
     public static int PosID = Shader.PropertyToID("_Position");
     public static int SizeID = Shader.PropertyToID("_Size");
-
-    public Material WallMaterial;
+    
+    public float SphereCastRadius = 1;
+    public float CircleSize = 2;
     public Camera Camera;
     public LayerMask Mask;
+
+    List<Material> _Materials;
+
+    private void Start()
+    {
+        _Materials = new List<Material>();
+    }
 
     void Update()
     {
         var dir = Camera.transform.position - transform.position;
         Ray ray = new Ray(transform.position, dir.normalized);
 
-        if(Physics.Raycast(ray, out RaycastHit hitinfo, 3000, Mask))
+        ClearMaterialList();
+        var hits = Physics.SphereCastAll(ray, SphereCastRadius, dir.magnitude - (SphereCastRadius * 2), Mask);
+        
+        if(hits.Length > 0)
         {
-            WallMaterial.SetFloat(SizeID, 0);
-            WallMaterial = hitinfo.collider.gameObject.GetComponent<MeshRenderer>().material;
-            WallMaterial.SetFloat(SizeID, 2);
+            foreach(var hit in hits)
+            {
+                Material mat = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
+                _Materials.Add(mat);
+                mat.SetFloat(SizeID, CircleSize);
+            }
         }
-        else
-        {
-            WallMaterial.SetFloat (SizeID, 0);
-        }
+        UpdateMatPos();
+    }
 
-        var view =  Camera.WorldToViewportPoint(transform.position);
-        WallMaterial.SetVector(PosID, view);
+    private void ClearMaterialList()
+    {
+        foreach(var mat in _Materials)
+        {
+            mat.SetFloat(SizeID, 0);
+        }
+        _Materials.Clear();
+    }
+
+    private void UpdateMatPos()
+    {
+        foreach(var mat in _Materials)
+        {
+            var view = Camera.WorldToViewportPoint(transform.position);
+            mat.SetVector(PosID, view);
+        }
     }
 }
+
