6d612bd150b8c54905d394ff879ef78a31d20dc9
Neebelwerfer <jakob.hedeby@gmail.com>
2024-03-06 01:06:57 +0100

    Added the new abililties to the player

--- Assets/Scripts/Abilities/Implementations/MeleeAttack.cs
+++ Assets/Scripts/Abilities/Implementations/MeleeAttack.cs
@@ -1,5 +1,6 @@
 using Character;
 using Character.Abilities;
+using Codice.Client.Commands;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Abilities/MeleeAttack")]
@@ -11,7 +12,9 @@ public class MeleeAttack : AttackAbility
 
     public override void CastStarted(CasterInfo caster)
     {
-        targets = TargetUtility.GetTargetsInLine(caster.owner.transform.position, caster.owner.transform.forward, new Vector3(width/2, 2, attackRange/2), targetMask);
+        var MousePoint = TargetUtility.GetMousePoint(Camera.main);
+        var dir = (MousePoint - caster.castPos).normalized;
+        targets = TargetUtility.GetTargetsInLine(caster.owner.transform.position, dir, new Vector3(width/2, 2, attackRange/2), targetMask);
     }
 
     public override void CastEnded(CasterInfo caster)
