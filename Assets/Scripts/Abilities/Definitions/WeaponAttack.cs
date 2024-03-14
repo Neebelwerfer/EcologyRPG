757b1788e9d6bef2f0df5721b1ea2f17e8fcaded
Neebelwerfer <jakob.hedeby@gmail.com>
2024-03-14 04:34:47 +0100

    Updated the targeting of an weapon attack to allow for 3 types: Cone Circular Line

--- Assets/Scripts/Abilities/Definitions/WeaponAttack.cs
+++ Assets/Scripts/Abilities/Definitions/WeaponAttack.cs
@@ -1,4 +1,13 @@
 public abstract class WeaponAttack : AttackAbility
 {
+    public enum TargetType
+    {
+        Line,
+        Cone,
+        Circular
+    }
+
     public float BaseDamage;
+    public TargetType targetType;
+    public bool useMouseDirection;
 }
--- Assets/Scripts/Abilities/Implementations/MeleeAttack.cs
+++ Assets/Scripts/Abilities/Implementations/MeleeAttack.cs
@@ -11,8 +11,14 @@ public class MeleeAttack : WeaponAttack
 
     public override void CastStarted(CasterInfo caster)
     {
+        var dir = useMouseDirection ? TargetUtility.GetMouseDirection(caster.owner.Position, Camera.main) : caster.owner.Forward;
 
-        targets = TargetUtility.GetTargetsInLine(caster.owner.Position, caster.owner.Forward, new Vector3(width/2, 2, Range/2), targetMask);
+        if(targetType == TargetType.Cone)
+            targets = TargetUtility.GetTargetsInCone(caster.owner.Position, dir, 45, Range, targetMask);
+        else if(targetType == TargetType.Line)
+            targets = TargetUtility.GetTargetsInLine(caster.owner.Position, dir, new Vector3(width/2, 2, Range/2), targetMask);
+        else if(targetType == TargetType.Circular)
+            targets = TargetUtility.GetTargetsInRadius(caster.owner.Position, Range, targetMask);
     }
 
     public override void CastEnded(CasterInfo caster)
