7345197a716a32c1a077ecd7a2967b036f2aef4f
Neebelwerfer <jakob.hedeby@gmail.com>
2024-03-22 02:36:23 +0100

    added some cleanup when removing abilities in via inspector ui

--- Assets/Scripts/Abilities/Implementations/CenteredExplosion.cs
+++ Assets/Scripts/Abilities/Implementations/CenteredExplosion.cs
@@ -42,7 +42,7 @@ public class CenteredExplosion : BaseAbility
         }
     }
 
-    private void OnDestroy()
+    protected override void OnDestroy()
     {
         foreach (var effect in OnHitEffects)
         {
