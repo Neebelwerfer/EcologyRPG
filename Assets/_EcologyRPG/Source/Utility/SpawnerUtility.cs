b7d677fc874a809db968987580490b3c20549a2c
Neebelwerfer <jakob.hedeby@gmail.com>
2024-04-13 03:49:02 +0200

    Last set of hotfixes

--- Assets/_EcologyRPG/Source/Utility/SpawnerUtility.cs
+++ Assets/_EcologyRPG/Source/Utility/SpawnerUtility.cs
@@ -4,8 +4,10 @@ public class SpawnerUtility : MonoBehaviour
 {
     public Mesh PlayerMesh;
 
+#if UNITY_EDITOR
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireMesh(PlayerMesh, transform.position, transform.rotation, transform.localScale);
     }
+#endif
 }
